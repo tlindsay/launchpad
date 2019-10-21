9cad95462e0984c8ada7b311b28cdb664ffbf203
Patrick Lindsay <patrick.lindsay@dockyard.com>
2019-10-21 11:31:14 -0500

    Bootstrap our components

--- app/components/weather-card.js
+++ app/components/weather-card.js
@@ -1,4 +1,26 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-});
+export default class WeatherCard extends Component {
+  @tracked latitude = this.position.latitude;
+  @tracked longitude = this.position.longitude;
+
+  get position() {
+    console.log('getting position')
+    return new Promise(function (resolve, reject) {
+      console.log('promise')
+      if (navigator) {
+        console.log('nav')
+        navigator.geolocation.getCurrentPosition(p => {
+          console.log('got position')
+          resolve({
+            latitude: p.coords.latitude,
+            longitude: p.coords.longitude
+          });
+        });
+      } else {
+        reject();
+      }
+    });
+  }
+}
