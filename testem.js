7cc5be9964f44e8c66c5193488fe7aee2314bebd
Patrick Lindsay <patrick.lindsay@dockyard.com>
2019-10-21 11:30:31 -0500

    ember init -b @ember/octane-app-blueprint

--- testem.js
+++ testem.js
@@ -12,6 +12,7 @@ module.exports = {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
         process.env.CI ? '--no-sandbox' : null,
+
         '--headless',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
