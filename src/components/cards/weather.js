63d3235b103c1f5c17d1a703568873a78ab59a82
Patrick Lindsay <patrick.lindsay@dockyard.com>
2021-04-08 10:53:31 -0500

    Add geolocation error handling
    NOTE: THIS IS SUPER OLD CODE THAT I'M COMMITTING NOW. NO IDEA IF IT'LL
    BREAK OR NOT

--- src/components/cards/weather.js
+++ src/components/cards/weather.js
@@ -25,6 +25,7 @@ const WeatherCard = () => {
   const [isLoading, setLoading] = useState(true);
   const [weather, setWeather] = useState(null);
   const loc = useGeolocation();
+  console.info('Weather loading')
 
   const getWeather = async ({ latitude, longitude }) => {
     let params = [
@@ -44,7 +45,14 @@ const WeatherCard = () => {
   };
 
   useEffect(() => {
-    if (loc.latitude === null) return;
+    if (loc.error) {
+      console.log('Location error: ', loc.error);
+      return
+    }
+    if (loc.latitude === null) {
+      console.error('Location unavailable');
+      return;
+    }
     getWeather(loc);
   }, [loc]);
 
