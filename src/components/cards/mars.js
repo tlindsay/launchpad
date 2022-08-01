53fd102852d1cd63495eab40a64a6456793861bc
Patrick Lindsay <pat@thatdarnpat.com>
2022-08-01 15:26:09 -0500

    Upgrade mars card to use orbiter.thatdarnpat.com

--- src/components/cards/mars.js
+++ src/components/cards/mars.js
@@ -1,21 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Card from '../card';
 
-const ENDPOINT = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos';
-const API_KEY = 'oi5Qw99OwyRKa43TPEquG2kbdKCd0eifOT5sA4uk';
-
-const randomNum = (min = 0, max) => {
-  return Math.floor(Math.random() * (max - min) + min);
-};
+const ENDPOINT = 'https://orbiter.thatdarnpat.com';
 
 const MarsCard = () => {
   const [isLoading, setLoading] = useState(true);
   const [mars, setMars] = useState(null);
 
   const getMars = async () => {
-    let resp = await fetch(`${ENDPOINT}?sol=${randomNum(940, 1200)}&camera=navcam&api_key=${API_KEY}`);
-    let json = await resp.json();
-    let [photo] = json.photos;
+    let resp = await fetch(ENDPOINT);
+    let photo = await resp.json();
     setMars({ src: photo.img_src, alt: photo.earth_date });
     setLoading(false);
   };
@@ -25,8 +19,14 @@ const MarsCard = () => {
   }, []);
 
   return (
-    <Card className={`mars ${isLoading && 'loading'}`}>
-      {isLoading ? 'Loading' : <img src={mars.src} alt={mars.alt} />}
+    <Card className={`mars ${isLoading ? 'loading' : ''}`}>
+      {
+        isLoading ? 'Loading' :
+          <figure>
+            <img src={mars.src} alt={mars.alt} />
+            <figcaption>{mars.alt}</figcaption>
+          </figure>
+      }
     </Card>
   );
 };
